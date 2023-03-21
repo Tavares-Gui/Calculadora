8276e3dae5a453c7d221b1c7c56a4ae17cca7773
Guilherme Tavares <guilherme.rtavares@outlook.com>
2023-03-21 10:13:57 -0300

    Finalizando

--- Main.py
+++ Main.py
@@ -1,11 +1,15 @@
-import felipe
+import guilherme, felipe
 
-x = int(input('Insira o valor de X: '))
-y = int(input('Insira o valor de Y: '))
+x = int (input("Insira o valor de X: "))
+y = int (input("Insira o valor de Y: "))
 
-escolha = int(input('Qual operação você deseja realizar?\n3- Multiplicação. 4- Divisão.'))
+escolha = int(input("Escolha a operação que deseja fazer:\n1 - Soma. 2 - Subtração 3 - Multiplicação. 4 - Divisão.\n"))
 
-if escolha == 3:
+if escolha == 1:
+    print("O resultado da soma é", guilherme.soma(x, y))
+elif escolha == 2:
+    print("O resultado da subtração é", guilherme.subtracao(x, y))
+elif escolha == 3:
     print('A multiplicação dos números escolhidos é:',felipe.multiplicacao(x,y))
 elif escolha == 4:
     print('A divisão dos números escolhidos é:',felipe.divisao(x,y))
